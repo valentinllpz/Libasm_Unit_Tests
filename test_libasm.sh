3344eba5439768ae37f89be986b5b011e7a0abb2
mli <mli@student.42.fr>
2020-06-04 14:08:37 +0200

    Compile according to  (+) Exit if can't compile (+) executable

--- test_libasm.sh
+++ test_libasm.sh
@@ -1,21 +1,34 @@
+#!/usr/bin/env bash
+
+libasm_path='../'
+incl_path=$libasm_path
+
 touch tmp{1..5}
 echo "For, after all, how do we know that two and two make four? Or that the force of gravity works? Or that the past is unchangeable? If both the past and the external world exist only in the mind, and if the mind itself is controllable – what then?" >> tmp6
-make
+
+if ! make bonus -C $libasm_path ; then
+	if ! make -C $libasm_path ; then
+		printf "\e[0;91m\t\tCOULD'T COMPILE !!\e[0m\n" && exit
+	fi
+fi
+if ! clang -Wall -Wextra -Werror -I $incl_path main.c -L $libasm_path -lasm ; then
+	printf "\e[0;91m\t\tCOULD'T COMPILE BINARY !!\e[0m\n" && exit
+fi
 clear
 printf "\e[1;35m"
-printf "#******************************************************************************#\n"
-printf "#                                                                              #\n"
-printf "#                   :::          :::                                           #\n"
-printf "#                  :+:      :+: :+:                                            #\n"
-printf "#                 +:+          +:+                                             #\n"
-printf "#                +#+      +#+ +#+#+#+  +#+#+#+  +#+#+#+ +#+#+#+#+              #\n"
-printf "#               #+#      #+# #+#  #+#     +#+# #+#+    #+#  +  #+#             #\n"
-printf "#              #+#      #+# #+#  #+# #+#  #+#    +#+# +#+  +  #+#              #\n"
-printf "#             ######## ### #######   ####### ####### ###  #  ###               #\n"
-printf "#                                                                              #\n"
-printf "#                                            Basic unit tests                  #\n"
-printf "#                                                                              #\n"
-printf "#******************************************************************************#\n\n"
+printf "# ****************************************************************************** #\n"
+printf "#                                                                                #\n"
+printf "#                    :::          :::                                            #\n"
+printf "#                   :+:      :+: :+:                                             #\n"
+printf "#                  +:+          +:+                                              #\n"
+printf "#                 +#+      +#+ +#+#+#+  +#+#+#+  +#+#+#+ +#+#+#+#+               #\n"
+printf "#                #+#      #+# #+#  #+#     +#+# #+#+    #+#  +  #+#              #\n"
+printf "#               #+#      #+# #+#  #+# #+#  #+#    +#+# +#+  +  #+#               #\n"
+printf "#              ######## ### #######   ####### ####### ###  #  ###                #\n"
+printf "#                                                                                #\n"
+printf "#                                             Basic unit tests                   #\n"
+printf "#                                                                                #\n"
+printf "# ****************************************************************************** #\n\n"
 printf "\e[0m"
 
 PS3='Welcome to this Libasm tester! Please enter your choice: '
@@ -25,32 +38,32 @@ do
     case $opt in
         "Test all")
             echo "Testing all..."
-			gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "0"
+			./a.out "0"
             break
             ;;
         "Test ft_strlen")
             echo "Testing only ft_strlen..."
-            gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "1"
+            ./a.out "1"
             ;;
         "Test ft_strcpy")
             echo "Testing only ft_strcpy..."
-            gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "2"
+            ./a.out "2"
             ;;
         "Test ft_strcmp")
             echo "Testing only ft_strcmp..."
-            gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "3"
+            ./a.out "3"
             ;;
         "Test ft_write")
             echo "Testing only ft_write..."
-            gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "4"
+            ./a.out "4"
             ;;
         "Test ft_read")
             echo "Testing only ft_read..."
-            gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "5"
+            ./a.out "5"
             ;;
         "Test ft_strdup")
             echo "Testing only ft_strdup..."
-            gcc -Wall -Wextra -Werror main.c -L .. -lasm && ./a.out "6"
+            ./a.out "6"
             ;;
         "Quit")
             break
@@ -59,4 +72,4 @@ do
     esac
 done
 rm tmp*
-make clean
+make clean -C $libasm_path >> /dev/null
